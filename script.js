2b2fdff2a595b5bb25601638b1e0171471d72462
A. Mambaus Sholihin <bauagg87@gmail.com>
2023-03-08 10:26:40 +0700

    baus

---
+++ script.js
@@ -0,0 +1,49 @@
+class table {
+    constructor(isiTabel) {
+        this.tabel = isiTabel;
+    }
+
+    createHaeder(data) {
+        let open = "<thead><tr>";
+        let closs = "</tr></thead";
+        data.forEach(function (d) {
+            open += `<th>${d}</th>`;
+        });
+
+        return open + closs;
+    }
+
+    createBody(data) {
+        let open = "<body>"
+        let closs = "</body"
+
+        data.forEach(function (d) {
+            open += `
+            <tr>
+            <td>${d[0]}</td>
+            <td>${d[1]}</td>
+            </tr>`;
+        })
+        return open + closs;
+    }
+
+    render(element) {
+        let table =
+            "<table class ='table table hover'>" +
+            this.createHaeder(this.tabel.colom) +
+            this.createBody(this.tabel.data) +
+            "</table>";
+        element.innerHTML = table;
+    }
+}
+
+const dataContent = new table({
+    colom: ["nama", "email"],
+    data: [
+        ["A. Mambaus Sholihin", "bagas1234@gmail.com"],
+        ["Melinda Aprilia Malik", "april1234@gmail.com"]
+    ]
+});
+
+const app = document.getElementById('app');
+dataContent.render(app);
