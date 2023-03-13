30ae77183fb8955eccd6ba2188fe743c4ccce8bc
A. Mambaus Sholihin <bauagg87@gmail.com>
2023-03-13 09:49:51 +0700

    baus

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// impor default
-import dataContent, { app } from "./moduleData";
-console.log(dataContent)
-console.log(app)
-
-// import object{}
-import { createHaeder, createBody, render } from "./moduleData"
-console.log(createHaeder(), createBody(), render())
